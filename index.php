03f446f1e752407c03985f6d33274669e709dcb3
GiacomoDiNardo <vest_95@hotmail.it>
2022-07-07 16:19:10 +0200

    compleated exercise

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+$text = "Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+         Possimus magnam incidunt eos ut maxime quo sit,
+         veritatis libero expedita quidem doloremque soluta modi non nesciunt, 
+         consequuntur temporibus, voluptatem vitae. Aliquid?
+        ";
+
+$search = $_GET["search"];
+
+echo $text;
+echo "<br>";
+echo "Lunghezza testo " . strlen($text);
+echo "<br>";
+if(stripos($text, $search) !== false) {
+    $text = str_ireplace($search, "***", $text);
+}
+
+echo $text;
+echo "<br>";
+echo "Lunghezza testo " . strlen($text);
+?>
